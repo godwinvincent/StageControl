9579ce8fec8949427b0b36628d1cb77e1718df95
Godwin Vincent <godwin@Godwins-MacBook.local>
2013-03-24 15:29:01 -0700

    Added Initial File

---
+++ StageControlMain.py
@@ -0,0 +1,60 @@
+'''
+Application example using build() + return
+==========================================
+
+An application can be build if you return a widget on build(), or if you set
+self.root.
+'''
+
+import kivy
+kivy.require('1.0.7')
+
+import ConfigParser
+
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.config import Config
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
+
+class TestApp(App):
+
+    def build(self):
+    	
+        def button_press(x):
+			print x.text;
+
+    	layout = FloatLayout(size=(300, 300))
+
+    	l_title = Label(text="Bellarmine College Prep",pos=(0,250), font_size='50sp')
+    	layout.add_widget(l_title)
+
+        p_title = Label(text="Noises Off",pos=(0,175), font_size='30sp')
+        layout.add_widget(p_title)
+
+    	b_scenePlus = Button(text='Scene +',size_hint=(.15, .1),pos=(550, 300))
+    	layout.add_widget(b_scenePlus)
+
+    	b_sceneMinus = Button(text='Scene -',size_hint=(.15, .1),pos=(550, 200))
+    	layout.add_widget(b_sceneMinus)
+
+    	glayout = GridLayout(cols=1, spacing=10, size_hint_y=None)
+		#Make sure the height is such that there is something to scroll.
+        glayout.bind(minimum_height=glayout.setter('height'))
+        for i in range(30):
+            btn = Button(text=str(i), size_hint_y=None, height=40)
+            btn.bind(on_release=button_press)
+            glayout.add_widget(btn)
+            
+        root = ScrollView(size_hint=(None, None), size=(400, 450))
+        root.add_widget(glayout)
+
+    	layout.add_widget(root)
+
+    	return layout
+        
+
+if __name__ == '__main__':
+    TestApp().run()
